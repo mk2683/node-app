dc13267e46bbd50538015ca6403bbee9a1df1a85
mohit kumar <mksujanian2683@gmail.com>
2018-10-04 13:47:36 +0530

    Add start script and remove config.json

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('./config/config');
-//require('./middlewares/auth/auth');
 
 var express    = require("express");
 var bodyParser = require("body-parser");
@@ -7,11 +6,9 @@ var {ObjectID} = require("mongodb");
 var _          = require("lodash");
 var bcrypt     = require("bcryptjs");
 var cors       = require("cors");
-//var methodOverride = require("method-override");
 
 var {mongoose} = require("./db/mongoose");
 var indexRoute  = require("./routes/index");
-var profileRoute = require("./routes/profile");
 
 var app = express();
 app.use(bodyParser.json());
@@ -19,10 +16,8 @@ app.use(cors());
 var port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use(methodOverride("_method"));
 
 app.use("/", indexRoute);
-app.use("/", profileRoute);
 
 app.listen(port, () => {
 	console.log(`Server has Started at port ${port}`);
